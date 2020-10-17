a5f9bdc3d92dd848307c7f210509d56565bfc79e
github username <github email address>
2020-10-17 16:30:55 -0600

    structure and components in place

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
+import ProductList from './components/ProductList';
+import Detail from './components/Detail';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-      <div className="col-6">column number one</div>
-      <div className="col-6"><span><i className="fas fa-home" /></span></div>
-      </div>
-    </div>
-  );
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <ProductList />
+        <Detail />
+        <Cart />
+        <Default />
+        
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Detail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Detail extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello From Details</h3>
+            </div>
+        )
+    }
+}
