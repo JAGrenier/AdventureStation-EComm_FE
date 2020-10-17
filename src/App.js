474d18f90ef2c13c69ec827a0cabd2426e874ca0
github username <github email address>
2020-10-17 16:51:30 -0600

    Router and Switch set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import ProductList from './components/ProductList';
-import Detail from './components/Detail';
+import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
 
@@ -16,11 +17,12 @@ export default class App extends Component {
     return (
       <React.Fragment>
         <NavBar />
-        <ProductList />
-        <Detail />
-        <Cart />
-        <Default />
-        
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
       </React.Fragment>
     );
   }
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Detail extends Component {
+export default class Details extends Component {
     render() {
         return (
             <div>
