4ceaa12f3f994d652e0ee1df7e9356a8b8f8dd30
github username <github email address>
2020-10-17 18:20:02 -0600

    changed color pallete to green/purple and moving on from nav-bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ import {ButtonContainer} from "./Button";
 export default class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 {/* { 
                 https://www.iconfinder.com/icons/1243689/call_phone_icon
                 Creative Commons (Attribution 3.0 Unported);
@@ -16,7 +16,7 @@ export default class NavBar extends Component {
                 <img src={logo} alt="store" className="navbar-brand" />
                 </Link>
                 <ul className="navbar-nav.align-items-center">  
-                    <li className="nav-itme ml-5">
+                    <li className="nav-item ml-5">
                         <Link to="/" className="nav-link">
                             products
                         </Link>
@@ -30,7 +30,17 @@ export default class NavBar extends Component {
                         my cart
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         );
     }
 }
+
+const NavWrapper = styled.nav `
+    background: var(--mainGreen);
+    .nav-link {
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform:capitalize;
+        
+    }
+`
