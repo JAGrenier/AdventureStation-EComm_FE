b9da85e7703572e8cf49d5fb5f2713aba93a5a61
github username <github email address>
2020-10-19 16:45:43 -0600

    start with modal

--- src/context.js
+++ src/context.js
@@ -7,7 +7,9 @@ class ProductProvider extends Component {
     state = {
         products: [], 
         detailProduct: detailProduct,
-        cart: []
+        cart: [], 
+        modalOpen: true,
+        modalProduct: detailProduct
     }
     componentDidMount(){
         this.setProducts();
@@ -48,6 +50,19 @@ class ProductProvider extends Component {
         }, () => {console.log(this.state)} )
     }
 
+    openModal = (id) => {
+        const product = this.getItem(id); 
+        this.setState(() => {
+            return {modalProduct: product, modalOpen:true}
+        })
+    }
+
+    closeModal = () => {
+        this.setState(() => {
+            return {modalOpen: false}
+        })
+    }
+
     render() {
         return (
             <ProductContext.Provider 
@@ -55,6 +70,8 @@ class ProductProvider extends Component {
                     ...this.state, 
                     handleDetail:this.handleDetail,
                     addToCart:this.addToCart,
+                    openModal:this.openModal,
+                    closeModal:this.closeModal
                 }}>
                 {this.props.children}
             </ProductContext.Provider>
