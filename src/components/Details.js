8876cf21524a9a59302f01b4df4a8415f3797b93
github username <github email address>
2020-10-19 10:53:55 -0600

    made add to cart button purple

--- src/components/Details.js
+++ src/components/Details.js
@@ -45,10 +45,12 @@ export default class Details extends Component {
                                         </ButtonContainer>
                                     </Link>
                                     <ButtonContainer 
+                                    cart 
                                     disabled={inCart ? true : false} 
                                     onClick={()=> {
                                         value.addToCart(id)
-                                    }}>
+                                    }}
+                                    >
                                         {inCart ? 'in cart' : 'add to cart'}
                                     </ButtonContainer>   
                                 </div>
