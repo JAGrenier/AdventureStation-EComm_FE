f13da38e4d29d1ddb8cbd8b771d6b89aa3310f7d
github username <github email address>
2020-10-20 14:03:24 -0600

    paypal sandbox

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,7 +17,7 @@ export default class NavBar extends Component {
                 </Link>
                 <ul className="navbar-nav.align-items-center">  
                     <li className="nav-item ml-5">
-                        <Link to="/" className="nav-link">
+                        <Link to="/Products" className="nav-link">
                             products
                         </Link>
                     </li>
